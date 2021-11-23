4b47104de4c010a7c3401b4e215017137df18d9a
misostack <misostack.com@gmail.com>
2021-11-23 17:48:42 +0700

    Add craco and tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,10 @@
 import './App.scss';
-import {
-  Input,
-  FormControl,
-  AppBox,
-  Grid,
-  InputLabel,
-  ButtonPrimary,
-  FormHelperText,
-} from './components';
 
 function BookMarkForm() {
   return (
     <>
-      <AppBox
-        title={'New Bookmark'}
-        sx={{ maxWidth: '640px', marginLeft: 'auto', marginRight: 'auto' }}
-      >
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <FormControl fullWidth>
-              <Input
-                id="bookmarkName"
-                aria-describedby="Your bookmark name"
-                placeholder="Name"
-              />
-              <FormHelperText
-                id="bookmarkNameHelperText"
-                style={{ marginLeft: 0 }}
-              >
-                You should use a meaningful name that can memorize the link
-              </FormHelperText>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12}>
-            <FormControl fullWidth>
-              <Input
-                id="bookmarkURL"
-                aria-describedby="Your bookmark URL"
-                placeholder="URL"
-              />
-              <FormHelperText
-                id="bookmarkURLHelperText"
-                style={{ marginLeft: 0 }}
-              >
-                It should be a URL that you wanna bookmark
-              </FormHelperText>
-            </FormControl>
-          </Grid>
-          <Grid item xs={12} sx={{ textAlign: 'right' }}>
-            <ButtonPrimary>SAVE</ButtonPrimary>
-          </Grid>
-        </Grid>
-      </AppBox>
+      <input type="text" name="name" /> <br />
+      <input type="text" name="url" />
     </>
   );
 }
